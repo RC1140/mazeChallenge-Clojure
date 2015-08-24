7aefa1b09639ec2e4e8fbc1fbde97910c2359aef
Jameel Haffejee <jameel@chasesoftware.co.za>
2015-08-24 20:32:59 +0200

    init

---
+++ test/codechallenge2/core_test.clj
@@ -0,0 +1,7 @@
+(ns codechallenge2.core-test
+  (:require [clojure.test :refer :all]
+            [codechallenge2.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
